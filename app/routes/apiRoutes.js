95b121fd5ad2c0a4055010e69f20b52c2a1b962a
Austin <elektricwebdesign@gmail.com>
2017-08-12 08:17:57 -0600

    added remaining questions back in and add git hub link. Also created get route for api/friends

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -11,27 +11,19 @@ router.post('/api/friends', (req, res) => {
   for(var i =0; i < friendsList.length; i +=1) {
     var totalDifference = 0;
     var scores = friendsList[i].scores.length
-    // console.log('WHAT IS OUR FRIENDS LIST',scores);
 
     for(var j = 0; j < 2; j +=1) {
-      // console.log(' WHAT IS OUR VALUES TO COMPARE', friendsList[i].scores[j], "New friend", newFriend.scores[j]);
       totalDifference +=  Math.abs(friendsList[i].scores[j] - newFriend.scores[j])
-      // console.log(' WHAT IS OUR FRIEND score', friendsList[i].scores[j]);
-      // console.log(' WHAT IS OUR NEW FRIEND SCORE', newFriend.scores[j]);
-      // console.log(' WHAT IS OUR TOTAL AS WE COMPARE', Math.abs(friendsList[i].scores[j] - newFriend.scores[j]));
-      // console.log("this is the compared values",  totalDifference);
 
     }
     results.push(totalDifference)
   }
-  // console.log(' WHAT ARE THE RESULTS ???', results);
   let bffIndex = Math.min.apply(null, results);
   let match = results.indexOf(bffIndex);
-  console.log('What is our total resuLTS', match, results);
-  console.log(' WHAT IS OUR BFF INDEX', friendsList[match]);
   let bff = friendsList[match]
-  // res.sendFile(path.join(__dirname, '../public/survey.html'));
-  // res.sendStatus(200);
   res.json(bff);
+});
+router.get('/api/friends', (req, res) => {
+  res.json(friendsList)
 })
 module.exports = router;
